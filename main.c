aeb8c2a5689c4920b6d9bd0fab29fcefe3daf31b
merce-arz <50120581+merce-arz@users.noreply.github.com>
2020-07-29 09:45:43 -0500

    Update main.c

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ void *escritor(void *wno)
 {
     sem_wait(&wrt);
     cnt = cnt*2;
-    printf("Escritor %d esta escribiendo",(*((int *)wno)),cnt);
+    printf("Escritor %d esta escribiendo \n",(*((int *)wno)),cnt);
     sem_post(&wrt);
 
 }
@@ -20,39 +20,39 @@ void *lector(void *rno)
     pthread_mutex_lock(&mutex);
     numreader++;
     if(numreader == 1) {
-        sem_wait(&wrt); /
+        sem_wait(&wrt);
     }
     pthread_mutex_unlock(&mutex);
-    printf("Lector %d: esta leyendo,*((int *)rno),cnt);
+    printf("Lector %d: esta leyendo \n",*((int *)rno),cnt);
     pthread_mutex_lock(&mutex);
     numreader--;
-    if(numreader == 0) 
-    {
-        sem_post(&wrt); 
+    if(numreader == 0) {
+        sem_post(&wrt);
     }
     pthread_mutex_unlock(&mutex);
 }
 
 int main()
 {
-    pthread_t lee[8],escribe[2];
+    pthread_t lee[8],escribe[6];
     pthread_mutex_init(&mutex, NULL);
     sem_init(&wrt,0,1);
-    int a[10] = {1,2,3,4,5,6,7,8,9,10}; 
-    for(int i = 0; i < 10; i++) {
+
+    int a[10] = {1,2,3,4,5,6,7,8,9,10};
+
+    for(int i = 0; i < 8; i++) {
         pthread_create(&lee[i], NULL, (void *)lector, (void *)&a[i]);
     }
-    for(int i = 0; i < 5; i++) {
+    for(int i = 0; i < 6; i++) {
         pthread_create(&escribe[i], NULL, (void *)escritor, (void *)&a[i]);
     }
 
-    for(int i = 0; i < 10; i++) {
+    for(int i = 0; i < 6; i++) {
         pthread_join(lee[i], NULL);
     }
-    for(int i = 0; i < 5; i++) {
+    for(int i = 0; i < 6; i++) {
         pthread_join(escribe[i], NULL);
     }
-
     pthread_mutex_destroy(&mutex);
     sem_destroy(&wrt);
 
